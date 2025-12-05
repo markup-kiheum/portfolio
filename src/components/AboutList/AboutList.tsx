a4f55a9728ec266ac1585927f0f1c99c067512d7
Kiheum <markup.kiheum@gmail.com>
2025-12-05 18:06:29 +0900

    Home(About) 페이지 문구 수정 및 컴포넌트 분리

---
+++ src/components/AboutList/AboutList.tsx
@@ -0,0 +1,20 @@
+import { ABOUT_ITEMS } from '@/lib/constants/about';
+
+import AboutItem from '../AboutItem';
+import styles from './AboutList.module.css';
+
+export default function AboutList() {
+  return (
+    <ul className={styles.questionList}>
+      {ABOUT_ITEMS.map((item) => {
+        return (
+          <AboutItem
+            key={item.question}
+            question={item.question}
+            answer={item.answer}
+          />
+        );
+      })}
+    </ul>
+  );
+}
